2551c601867c9af81d893d3a6757bea650505eb3
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-11-12 11:21:44 +0530

    fix: added error logs for db

--- unite/packages/db/src/index.ts
+++ unite/packages/db/src/index.ts
@@ -2,4 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
+client.$connect().then(()=>{
+    console.log("DB connected")
+}).catch((e)=>{
+    console.log("Error connecting db")
+    console.log(e)
+})
 export default client; 
