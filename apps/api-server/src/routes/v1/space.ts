7dca1d035233cb673977f3bd9353a1bdd011bfa0
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-11-13 18:19:13 +0530

    fix: the fixed the api-server folder not fount

--- apps/api-server/src/routes/v1/space.ts
+++ apps/api-server/src/routes/v1/space.ts
@@ -2,13 +2,10 @@ import express from 'express'
 import { authUser } from '../../middleware/Authuser';
 import { addElementToSpace, createSpace, deleteSpace, getAllSpaces, getSpaceById, removeElementFromSpace } from '../../controller/space';
 const router = express.Router()
-
 router.post("/",authUser,createSpace)
 router.delete("/element",authUser,removeElementFromSpace) 
 router.delete("/:spaceId",authUser,deleteSpace)
 router.get("/all",authUser,getAllSpaces)
 router.get("/:spaceId",authUser,getSpaceById)
 router.post("/element",authUser,addElementToSpace) 
-
-
 export default router;
