27e1295843d28afe1cf5d023641fc152e29a7653
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-11-13 18:06:16 +0530

    fix: repo name

--- apps/api-server/src/routes/v1/space.ts
+++ apps/api-server/src/routes/v1/space.ts
@@ -8,7 +8,6 @@ router.delete("/element",authUser,removeElementFromSpace)
 router.delete("/:spaceId",authUser,deleteSpace)
 router.get("/all",authUser,getAllSpaces)
 router.get("/:spaceId",authUser,getSpaceById)
-
 router.post("/element",authUser,addElementToSpace) 
 
 
